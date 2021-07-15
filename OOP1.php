c366a2f5dbe78931391a8e0a97cfbe3cc4fef75a
Md Yamin Arafath <86312409+orthy5008@users.noreply.github.com>
2021-07-15 11:56:46 +0600

    Create OOP1.php

---
+++ OOP1.php
@@ -0,0 +1,12 @@
+<?php
+class product{
+      public $name;
+      public $description;
+      public $price;
+}
+$product1 = new product;
+echo $product1-> name = 'iPhone 12';
+echo PHP_EOL;
+$product2 = new product;
+echo $product2 -> name = 'iPhone 12 Pro';
+?>
